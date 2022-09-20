ded3f7c7921b50f19dbc92eac22b31942cb9015b
jrlucas1 <joaolucas.pl151@academico.ifsul.edu.br>
2022-09-20 19:23:45 -0300

    Mudanças na identação

--- alterarSenha.php
+++ alterarSenha.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html>
 <?php
- include_once('includes/componentes/cabecalho.php');
- include_once('includes/componentes/header.php');
+include_once('includes/componentes/cabecalho.php');
+include_once('includes/componentes/header.php');
 ?>
-    <title>Alterar Usuário</title>
+<title>Alterar Usuário</title>
 </head>
+
 <body>
-<?php ?>
-<main>
-    <section>
-    <form action="includes/logica/logica_pessoa.php" method="post">
-      <p><label for="email">Senha: </label><input type="password" name="senha" id="senha"></p>
-      <input type="hidden" id='idusuarios' name='idusuarios' value="<?php echo $_SESSION['idusuarios']; ?>">
-      <p> <input type="submit" id='alterarSenha' name='alterarSenha' value="Alterar senha">
-      </p>        
-        </form>
-    </section>
-</main>
-<?php require('includes/componentes/footer.php');?>
+    <?php ?>
+    <main>
+        <section>
+            <form action="includes/logica/logica_pessoa.php" method="post">
+                <p><label for="email">Senha: </label><input type="password" name="senha" id="senha"></p>
+                <input type="hidden" id='idusuarios' name='idusuarios' value="<?php echo $_SESSION['idusuarios']; ?>">
+                <p> <input type="submit" id='alterarSenha' name='alterarSenha' value="Alterar senha">
+                </p>
+            </form>
+        </section>
+    </main>
+    <?php require('includes/componentes/footer.php'); ?>
 </body>
+
 </html>
